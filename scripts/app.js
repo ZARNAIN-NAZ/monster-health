fad4d0cfda40a92b73a366edbbbaf7427d4f5206
ZARNAIN-NAZ <zarnainabid004@gmail.com>
2023-07-14 03:04:10 +0500

    STRONG BUTTON UPDTAE

--- scripts/app.js
+++ scripts/app.js
@@ -1,21 +1,56 @@
-const ATTACK_VALUE      = 10;
+const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE =17; 
+const MONSTER_ATTACK =14;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
-let  currentPlayerHealth= chosenMaxLife ; 
+let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
-
-function attackHandler(){
- const damage  = dealMonsterDamage(ATTACK_VALUE)
- currentMonsterHealth -= damage;
-
- if(currentMonsterHealth <=0){
-    alert("you won")
-
- }
- 
+function attackMonster(mode){
+  let maxDamage;
+if(mode==="ATTACK")
+{
+maxDamage = ATTACK_VALUE;
+}else if(mode === "STRONG_ATTACK"){
+  maxDamage = STRONG_ATTACK_VALUE;
+}
 }
 
 
-attackBtn.addEventListener("click" , attackHandler )
+function attackHandler() {
+  const damage = dealMonsterDamage(ATTACK_VALUE);
+  currentMonsterHealth -= damage;
+  const playerDamage= dealPlayerDamage(MONSTER_ATTACK)
+  currentPlayerHealth -=playerDamage;
+  if(currentMonsterHealth <= 0 && currentPlayerHealth >0){
+    alert("you won...");
+
+  }
+  else if(currentPlayerHealth<=0  && currentMonsterHealth >0){
+    alert("u lost")
+  }
+  else if (currentPlayerHealth <=0 && currentMonsterHealth <=0){
+    alert("you have a draw!")
+  }
+}
+
+function strongAttackHandler(){
+    const damage = dealMonsterDamage(STRONG_ATTACK_VALUE);
+    currentMonsterHealth -= damage;
+    const playerDamage= dealPlayerDamage(MONSTER_ATTACK)
+    currentPlayerHealth -=playerDamage;
+    if(currentMonsterHealth <= 0 && currentPlayerHealth >0){
+      alert("you won...");
+  
+    }
+    else if(currentPlayerHealth<=0  && currentMonsterHealth >0){
+      alert("u lost")
+    }
+    else if (currentPlayerHealth <=0 && currentMonsterHealth <=0){
+      alert("you have a draw!")
+    }
+}
+attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
+
--- scripts/vendor.js
+++ scripts/vendor.js
@@ -3,7 +3,7 @@ const playerHealthBar = document.getElementById("player-health")
 const bonusLifeEl = document.getElementById("bonus-life")
 
 const attackBtn = document.getElementById("attack-btn");
-const strongAttackBtn = document.getElementById("strong-btn");
+const strongAttackBtn = document.getElementById("strong-attack-btn");
 const healBtn = document.getElementById("heal-btn");
 const logBtn = document.getElementById("log-btn");
 
